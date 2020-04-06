76ada118a40a5f58f825ee753a56f9c2d5c40f3b
Nxtra <nickvanhoof1@gmail.com>
2020-04-06 18:54:35 +0200

    implement transform XML to JSON

--- src/RetrieveXMLTrafficData/handler.py
+++ src/RetrieveXMLTrafficData/handler.py
@@ -12,6 +12,7 @@ s3_client = boto3.client('s3')
 
 URL = os.environ["TRAFFIC_DATA_URL"]
 BUCKET = os.environ["BUCKET_NAME"]
+S3_XML_PREFIX = os.environ["S3_XML_PREFIX"]
 
 
 def retrieve_data():
@@ -19,12 +20,9 @@ def retrieve_data():
     return r.text
 
 
-def upload_data(data, file_name, bucket, object_name=None):
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = file_name
+def store_data(data, object_key, bucket):
     try:
-        response = s3_client.put_object(Body=data, Bucket=bucket, Key=object_name, ContentType='application/xml')
+        response = s3_client.put_object(Body=data, Bucket=bucket, Key=object_key, ContentType='application/xml')
     except ClientError as e:
         logging.error(e)
         return False
@@ -35,8 +33,8 @@ def handle(event, context):
     xml_data = retrieve_data()
 
     now = str(int(round(time.time() * 1000)))
-    key = 'xml/input/' + now + '.xml'
-    upload_data(xml_data, key, BUCKET)
+    key = S3_XML_PREFIX + now + '.xml'
+    store_data(xml_data, key, BUCKET)
 
     response = {
         "statusCode": 200,
--- src/TransformTrafficDataToJson/handler.py
+++ src/TransformTrafficDataToJson/handler.py
@@ -1,17 +1,59 @@
 import json
+import boto3
+import os
+import xmltodict
+import logging
+from botocore.exceptions import ClientError
+
+
+s3_resource = boto3.resource('s3')
+s3_client = boto3.client('s3')
+
+BUCKET = os.environ["BUCKET_NAME"]
+S3_XML_PREFIX = os.environ["S3_XML_PREFIX"]
+S3_JSON_PREFIX = os.environ["S3_JSON_PREFIX"]
 
 
 def handle(event, context):
-    print(event)
+    s3_event = event["Records"][0]
+    object_key = s3_event["s3"]["object"]["key"]
 
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    xml_data = get_from_s3(object_key)
+    json_data = convert_xml_to_dict(xml_data)
+    json_object_key = create_new_key(object_key)
+    store_data(json_data, json_object_key, BUCKET)
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps('success!')
     }
+    print('returning response')
     return response
+
+
+def get_from_s3(key):
+    obj = s3_resource.Object(BUCKET, key)
+    return obj.get()['Body'].read().decode('utf-8')
+
+
+def convert_xml_to_dict(xml_data):
+    my_dict = xmltodict.parse(xml_data)
+    json_data = json.dumps(my_dict)
+    return json_data
+
+
+def create_new_key(previous_key):
+    file_name = previous_key.replace(S3_XML_PREFIX, '')
+    file_name_without_suffix = os.path.splitext(file_name)[0]
+    new_json_object_key = S3_JSON_PREFIX + file_name_without_suffix + '.json'
+    return new_json_object_key
+
+
+def store_data(data, object_key, bucket):
+    try:
+        response = s3_client.put_object(Body=data, Bucket=bucket, Key=object_key, ContentType='application/json')
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
