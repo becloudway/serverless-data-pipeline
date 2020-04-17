f3bc11038a1af6145c309565028c7ce42a3023f6
Nxtra <nickvanhoof1@gmail.com>
2020-04-17 16:16:43 +0200

    add script to scan dynamo

---
+++ scripts/scan_all.py
@@ -0,0 +1,14 @@
+import boto3
+dynamodb = boto3.resource('dynamodb', region_name="eu-west-1")
+
+table = dynamodb.Table('sls-data-pipelines-dev-AnalyticsResultsTable-TC5J0ZRQDWBF')
+
+response = table.scan()
+data = response['Items']
+
+while 'LastEvaluatedKey' in response:
+    response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+    data.extend(response['Items'])
+
+print(len(data))
+print(data[0])
