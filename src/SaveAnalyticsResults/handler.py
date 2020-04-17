86d6e60ec593e02fea20137942b1e99296ba6a7a
Nxtra <nickvanhoof1@gmail.com>
2020-04-17 16:58:27 +0200

    implement dynamodb batch save

--- src/SaveAnalyticsResults/handler.py
+++ src/SaveAnalyticsResults/handler.py
@@ -12,31 +12,34 @@ def handle(event, context):
     print("START----------------------------------------------START")
 
     print(event)
-    for record in event['Records']:
-        try:
-            payload = base64.b64decode(record['kinesis']['data'])
-            data = json.loads(payload)
-            print(json.dumps(data))
-            print('Handling item')
-            item = None
-            if data.get('outputType') == 'SPEED_DIFFERENTIAL':
-                print('type is speed')
-                item = create_speed_item(data)
-            elif data.get('outputType') == 'TRAFFIC_JAM':
-                print('type is jam')
-                item = create_traffic_jam_item(data)
-            print('Saving item {}'.format(json.dumps(item)))
-            table.put_item(Item=item)
-            print('Item successfully saved')
-        except Exception as e:
-            print(e)
-            print("failure")
+    with table.batch_writer() as batch:
+        for record in event['Records']:
+            try:
+                payload = base64.b64decode(record['kinesis']['data'])
+                data = json.loads(payload)
+                print(json.dumps(data))
+                print('Handling item')
+                item = None
+                if data.get('outputType') == 'SPEED_DIFFERENTIAL':
+                    print('type is speed')
+                    item = create_speed_item(data)
+                elif data.get('outputType') == 'TRAFFIC_JAM':
+                    print('type is jam')
+                    item = create_traffic_jam_item(data)
+                sort_key = create_sort_key(item)
+                item["outputType_recordTimestamp"] = sort_key
+
+                print('Saving item {}'.format(json.dumps(item)))
+                batch.put_item(Item=item)
+                print('Item successfully saved')
+            except Exception as e:
+                print(e)
+                print("failure")
 
     print("END----------------------------------------------END")
 
 
 def create_speed_item(data_item):
-    # outputType_timestamp = data_item.get('outputType') + '_' + timestamp
     speed_item = {
         'uniqueId': str(data_item.get('uniqueId')),
         'recordTimestamp': str(data_item.get('recordTimestamp')),
@@ -60,3 +63,10 @@ def create_traffic_jam_item(data_item):
     }
     print('Returning jam item {}'.format(json.dumps(traffic_jam_item)))
     return traffic_jam_item
+
+
+def create_sort_key(item):
+    output_type = item.get('outputType')
+    record_timestamp = item.get('recordTimestamp')
+    sort_key = output_type + '_' + record_timestamp
+    return sort_key
