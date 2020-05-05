4bb015543bb103039b3dcb3ece0f549c199519c7
Nxtra <nickvanhoof1@gmail.com>
2020-05-05 22:13:32 +0200

    Get situation overview as list of locations

--- src/CurrentSituation/handler.py
+++ src/CurrentSituation/handler.py
@@ -11,13 +11,30 @@ table = dynamodb_resource.Table(os.environ["TABLE_NAME"])
 
 def handle(event, context):
     dynamodb_items = scan_table()
-    data = [parse_dynamo_item_to_dict(item) for item in dynamodb_items]
-    print(data)
+    items = [parse_dynamo_item_to_dict(item) for item in dynamodb_items]
+    print(items)
+    trafficLocations = create_traffic_locations_list(items)
+
+    response_body = {
+        "trafficLocations": trafficLocations
+    }
     response = {
         "statusCode": 200,
-        "body": json.dumps(data)
+        "body": json.dumps(response_body)
     }
     return response
+
+
+def create_traffic_locations_list(items):
+    print(items)
+    unique_ids = list(set([d["uniqueId"] for d in items]))
+    trafficLocations = []
+    for id in unique_ids:
+        data = {
+            id: [i for i in items if i["uniqueId"] == id]
+        }
+        trafficLocations.append(data)
+    return trafficLocations
 
 
 def scan_table(input={}):
@@ -33,13 +50,12 @@ def create_scan_input(table_name):
     return {
         "TableName": table_name,
         "ProjectionExpression": "#a97a0,#a97a1",
-        "ExpressionAttributeNames": {"#a97a0":"uniqueId", "#a97a1":"outputType"}
+        "ExpressionAttributeNames": {"#a97a0": "uniqueId", "#a97a1": "outputType"}
     }
 
 
 def parse_dynamo_item_to_dict(item):
     return json.loads(json.dumps(item, indent=4, cls=DecimalEncoder))
-
 
 
 # Helper class to convert a DynamoDB item to JSON.
